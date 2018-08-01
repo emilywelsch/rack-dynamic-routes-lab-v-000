f5439e3447b40526f18b345caec383c29ae3dd40
Emily Welsch <emwelsch@gmail.com>
2018-08-01 02:26:18 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,16 +1,16 @@
 class Application
- 
+
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
- 
+
     if req.path=="/items/#{Item.name}"
       resp.write "#{Item.price}"
     else
       resp.write "Route not found"
       resp.status = 404
     end
- 
+
     resp.finish
   end
 end
